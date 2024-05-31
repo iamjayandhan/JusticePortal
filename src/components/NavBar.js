d80f82b0466a144d0258a95a7c2604aaf477f824
Jayandhan <jayandhanrajendran03@gmail.com>
2024-05-31 23:06:20 +0530

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -52,7 +52,7 @@ function NavBar() {
           <li><Link to="/MainPage" onClick={handleMenuItemClick}>Home</Link></li>
           <li><Link to="/add-case" onClick={handleMenuItemClick}>Add Case</Link></li>
           {userRole !== 'Owners/Clients' && <li><Link to="/all-cases" onClick={handleMenuItemClick}>All Cases</Link></li>}
-          {userRole === 'Owners/Clients' && <li><Link to="/lawyers" onClick={handleMenuItemClick}>Lawyers</Link></li>}
+          <li><Link to="/lawyers" onClick={handleMenuItemClick}>Lawyers</Link></li>
           <li><Link to="/my-cases" onClick={handleMenuItemClick}>My Cases</Link></li>
           <li><Link to="/inbox" onClick={handleMenuItemClick}>Inbox</Link></li>
           <li><Link to="/profile" onClick={handleMenuItemClick}>Profile</Link></li>
