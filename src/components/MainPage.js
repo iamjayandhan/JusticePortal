5719fb60f3c6c780738c2f0367316308f2b8d5ec
Jayandhan <jayandhanrajendran03@gmail.com>
2024-06-08 22:50:05 +0530

    ui

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,9 +4,9 @@ import court1 from './Assets/bans2.png';
 import court2 from './Assets/bans1.png';
 import court3 from './Assets/bans8.png';
 import Cookies from 'js-cookie';
-import image1 from './Assets/img.png';
-import image2 from './Assets/img2.png';
-import image3 from './Assets/img3.png';
+import image1 from './Assets/supreme.png';
+import image2 from './Assets/Lawyer-bro.png';
+import focusImg from './Assets/Judge-bro.png'
 
 function MainPage() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -43,8 +43,8 @@ function MainPage() {
 
   return (
     <div className='mainpage-container'>
-      <header className='mainpage-header'>
-        <div className='carousel' style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
+      {/* <header className='mainpage-header'> */}
+        {/* <div className='carousel' style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
           {slides.map((slide, index) => (
             <div className='carousel-slide' key={index}>
               <img src={slide} alt={`Justice Portal Banner ${index + 1}`} />
@@ -52,15 +52,19 @@ function MainPage() {
           ))}
         </div>
         <button className='carousel-button prev' onClick={handlePrev}>‹</button>
-        <button className='carousel-button next' onClick={handleNext}>›</button>
-      </header>
+        <button className='carousel-button next' onClick={handleNext}>›</button> */}
+       
+      {/* </header> */}
+      <div className='focus-img'>
+              <img src={focusImg} alt='' className='mission-image-img' />
+            </div>
       
       <div className="first-container">
         <h1 className='greet'>Hello {username}!</h1>
         <h1 className='main-title'>The Realm of Justice</h1>
         <div>
           <p className='main-description'>
-            <strong>Enter the Realm of Justice</strong>, where legal efficiency meets technological innovation. Our platform revolutionizes case management processes by providing a centralized hub for seamless scheduling, real-time tracking, and comprehensive monitoring of various types of cases across diverse legal domains.
+            <strong>Enter the Realm of Justice</strong>, where legal efficiency meets technological innovation. <br />Our platform revolutionizes case management processes by providing a centralized hub for seamless scheduling, real-time tracking, and comprehensive monitoring of various types of cases across diverse legal domains.
           </p>
           <p className='main-description'>
             Join us in our <strong>mission to enhance the efficiency and effectiveness of judicial proceedings</strong> while upholding the principles of fairness and access to justice for all. Together, let's shape the future of legal practice.
@@ -70,6 +74,9 @@ function MainPage() {
 
       <div className="second-container">
         <section className='features-section'>
+        <div className='focus-img'>
+              <img src={image1} alt='' className='mission-image-img' />
+            </div>
           <h2 className='section-title'>OUR KEY FEATURES</h2>
           <div className='features-list'>
             <div className='feature-item'>Automated Case Management</div>
@@ -83,21 +90,13 @@ function MainPage() {
 
       <div className="last-container">
         <section className='mission-section'>
+        <div className='focus-img'>
+              <img src={image2} alt='' className='mission-image-img' />
+            </div>
           <h2 className='section-title'>OUR MISSION</h2>
           <p className='mission-description'>
             Our mission is to enhance the efficiency and effectiveness of judicial proceedings while upholding the principles of fairness and access to justice for all.
           </p>
-          <div className='mission-images'>
-            <div className='mission-image'>
-              <img src={image1} alt='' className='mission-image-img' />
-            </div>
-            <div className='mission-image'>
-              <img src={image2} alt='' className='mission-image-img' />
-            </div>
-            <div className='mission-image'>
-              <img src={image3} alt='' className='mission-image-img' />
-            </div>
-          </div>
         </section>
       </div>
 
